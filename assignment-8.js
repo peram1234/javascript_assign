accf9e1c184e1507a014f5ee9e0d3b80b98f695b
peram1234 <112181006+peram1234@users.noreply.github.com>
2022-09-28 22:24:57 +0530

    Add files via upload

---
+++ assignment-8.js
@@ -0,0 +1,16 @@
+1. We can store only unique values in set
+2.
+a) syntax for creating set
+ var set=new Set();
+b) adding values to set
+ console.log(set.add("A"));
+ console.logset.add('a');
+c) deleting values from set
+ console.log(set.delete("A"));
+ console.log(set.delete('a'));
+
+
+3.
+
+ var set=new Set([2,5,3,7]);
+ console.log(set.has(8));
